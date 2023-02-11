b88ba47585b5e08d87de95e09300d8037db30c0d
pavanipavani <116136788+pavanipavani@users.noreply.github.com>
2023-02-11 20:20:33 +0530

    I have written masai journey

---
+++ unit-1.js
@@ -0,0 +1,4 @@
+1.good evening mam,first of all thanks for giving this offerchunuty;
+2.I am in joining before masai i am so fear.But i could join in masai easly i can solved i assignments
+3. connect with other state persons and improving my communication skills.
+4.thanks for condacting  zoom marathon
